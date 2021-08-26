951d0f7591fe6232ace55b32da785902ecfd4711
snowyaya <75382121+snowyaya@users.noreply.github.com>
2021-08-26 14:44:01 -0400

    Create [Recursion] 98 Validate Binary Search Tree.java

---
+++ src/[Recursion] 98 Validate Binary Search Tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+    
+    public boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null) return true;
+        if (min!= null && min.val >= root.val) return false;
+        if (max != null && max.val <= root.val) return false;
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+}
