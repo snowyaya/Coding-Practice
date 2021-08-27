a3ffb718b894a4c6ec36392606d2a7c04c8f1d17
snowyaya <75382121+snowyaya@users.noreply.github.com>
2021-08-26 21:36:58 -0400

    Update [Array] 747 Largest Number At Least Twice of Others.java

--- src/[Array] 747 Largest Number At Least Twice of Others.java
+++ src/[Array] 747 Largest Number At Least Twice of Others.java
@@ -1,5 +1,56 @@
 // Merge sort first the compare
-
+class Solution {
+    public int dominantIndex(int[] nums) {
+        if (nums == null || nums.length < 1) return -1;
+        if (nums.length == 1) return 0;
+        int maxIdx = 0;
+        
+        int[] copyNums = Arrays.copyOfRange(nums, 0, nums.length);
+        
+        int[] sortedNums = mergeSort(copyNums);
+        if (sortedNums[sortedNums.length - 1] >= 2*sortedNums[sortedNums.length - 2]) {
+            for (int i = 0; i < nums.length; i++) {
+                if (sortedNums[sortedNums.length - 1] == nums[i]) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+        
+    }
+    
+    private int[] mergeSort(int[] copyNums) {
+        if (copyNums.length <= 1) return copyNums;
+        int pivot = copyNums.length / 2;
+        int[] leftArray = mergeSort(Arrays.copyOfRange(copyNums, 0, pivot));
+        int[] rightArray = mergeSort(Arrays.copyOfRange(copyNums, pivot, copyNums.length));
+        
+        return merge(leftArray, rightArray);
+    }
+    
+    private int[] merge(int[] leftArray, int[] rightArray) {
+        int[] result = new int[leftArray.length + rightArray.length];
+        int leftPtr = 0, rightPtr = 0, resultPtr = 0;
+        
+        // Merger two arrays recursively
+        while (leftPtr < leftArray.length && rightPtr < rightArray.length) {
+            if (leftArray[leftPtr] < rightArray[rightPtr]) {
+                result[resultPtr++] = leftArray[leftPtr++];
+            } else {
+                result[resultPtr++] = rightArray[rightPtr++];
+            }
+        }
+        
+        // Append what's left in the given arrays
+        while (leftPtr < leftArray.length) {
+            result[resultPtr++] = leftArray[leftPtr++];
+        }
+        while (rightPtr < rightArray.length) {
+            result[resultPtr++] = rightArray[rightPtr++];
+        }
+        return result;
+    }
+}
 
 // Iteration
 class Solution {
