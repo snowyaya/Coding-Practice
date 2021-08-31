1d40465d7c706174520b57b903bd0fd334be2078
snowyaya <75382121+snowyaya@users.noreply.github.com>
2021-08-30 21:08:55 -0400

    Create [Array] 53 Maximum Subarray.java

---
+++ src/[Array] 53 Maximum Subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        int max = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum < 0) {
+                sum = nums[i];
+            } else {
+                sum += nums[i];
+            }
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
