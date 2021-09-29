44c3693f5aa2524274153587d0903e84d643f0e1
snowyaya <75382121+snowyaya@users.noreply.github.com>
2021-09-28 21:52:39 -0400

    Update [BFS] 490 The Maze.java

--- src/[BFS] 490 The Maze.java
+++ src/[BFS] 490 The Maze.java
@@ -22,7 +22,7 @@ class Solution {
                 //Keep moving in a particular direction until we have met an obstacle or we're out of the maze.
                 int x = currPos[0] + d[0];
                 int y = currPos[1] + d[1];
-				while(x >= 0 && y >= 0 && x < m && y < n && maze[x][y]==0) {
+		while(x >= 0 && y >= 0 && x < m && y < n && maze[x][y]==0) {
                     x += d[0];
                     y += d[1];
                 }
