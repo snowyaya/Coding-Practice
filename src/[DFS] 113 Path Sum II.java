e3b0e00487cbc05ff4352637a76b70367edbed8a
snowyaya <75382121+snowyaya@users.noreply.github.com>
2021-09-29 22:38:07 -0400

    Update [DFS] 113 Path Sum II.java

--- src/[DFS] 113 Path Sum II.java
+++ src/[DFS] 113 Path Sum II.java
@@ -35,45 +35,6 @@ class Solution {
         }
         getPath(root.left, targetSum, list);
         getPath(root.right, targetSum, list);
-        list.remove(list.size() - 1); /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    List<List<Integer>> res = new ArrayList<>();
-    
-    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        if (root == null) return res;
-        
-        List<Integer> list = new ArrayList<>();
-        getPath(root, targetSum, list);
-        return res;
-    }
-    
-    public void getPath(TreeNode root, int targetSum, List<Integer> list) {
-        if (root == null) return;
-        
-        targetSum -= root.val;
-        list.add(root.val);
-        
-        if (root.left == null && root.right == null && targetSum == 0) {
-            res.add(new ArrayList<Integer>(list));
-        }
-        getPath(root.left, targetSum, list);
-        getPath(root.right, targetSum, list);
         list.remove(list.size() - 1); // remove the last element if the targetSum != 0
-    }
- }
     }
  }
